eb954f6abfd3081aef1fe85fc6e99e1c1b1c29a1
Olivier CHARON <olivier.charon@outlook.fr>
2023-06-21 17:48:15 +0200

    adding the posibility to call all the articles at once

--- app/Http/Controllers/Api/PostController.php
+++ app/Http/Controllers/Api/PostController.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Http\Controllers\Api;
+
 use Exception;
 use App\Models\Post;
 use Illuminate\Http\Request;
@@ -14,37 +15,38 @@ class PostController extends Controller
     {
         try {
             // $query = Post::query();
-            $perPage = $request->input('perPage',3);
-            $page = $request->input('page',1);
+            $perPage = $request->input('perPage', 3);
+            $page = $request->input('page', 1);
             $title = $request->input('title');
             $txt = $request->input('txt');
-            $query = Post::where('title','LIKE','%'.$title.'%')->where('txt','LIKE','%'.$txt.'%');
+            $query = Post::where('title', 'LIKE', '%' . $title . '%')->where('txt', 'LIKE', '%' . $txt . '%');
             $count = $query->count();
-            $lastPage = ceil($count/$perPage);
-            $page>$lastPage?$page=$lastPage:'';
-            $result = $query->offset(($page-1)*$perPage)->limit($perPage)->get();
+            $perPage === 'all' ? $perPage = $count : '';
+            $lastPage = ceil($count / $perPage);
+            $page > $lastPage ? $page = $lastPage : '';
+            $result = $query->offset(($page - 1) * $perPage)->limit($perPage)->get();
 
             return response()->json([
-                'status'=>200,
-                'message'=>'Posts found',
-                'current_page'=>$page,
-                'last_page'=>$lastPage,
-                'data'=>$result
+                'status' => 200,
+                'message' => 'Posts found',
+                'current_page' => $page,
+                'last_page' => $lastPage,
+                'data' => $result
             ]);
-        } catch (Exception $e){
+        } catch (Exception $e) {
             return response()->json($e);
         }
     }
-    
+
     public function show(Post $post)
     {
         try {
             return response()->json([
-                'status'=>200,
-                'message'=>'Post found',
-                'data'=>$post
+                'status' => 200,
+                'message' => 'Post found',
+                'data' => $post
             ]);
-        } catch (Exception $e){
+        } catch (Exception $e) {
             return response()->json($e);
         }
     }
@@ -57,17 +59,17 @@ class PostController extends Controller
             $post->img = $request->img;
             $post->txt = $request->txt;
             $post->save();
-            
+
             return response()->json([
-                'status'=>201,
-                'message'=>'Post created',
-                'data'=>$post
+                'status' => 201,
+                'message' => 'Post created',
+                'data' => $post
             ]);
-        } catch (Exception $e){
+        } catch (Exception $e) {
             return response()->json($e);
         }
     }
-    
+
     public function update(EditPostRequest $request, Post $post)
     {
         try {
@@ -75,13 +77,13 @@ class PostController extends Controller
             $post->img = $request->img;
             $post->txt = $request->txt;
             $post->save();
-            
+
             return response()->json([
-                'status'=>200,
-                'message'=>'Post updated',
-                'data'=>$post
+                'status' => 200,
+                'message' => 'Post updated',
+                'data' => $post
             ]);
-        } catch (Exception $e){
+        } catch (Exception $e) {
             return response()->json($e);
         }
     }
@@ -92,11 +94,10 @@ class PostController extends Controller
             $post->delete();
 
             return response()->json([
-                'status'=>200,
-                'message'=>'Post deleted'
+                'status' => 200,
+                'message' => 'Post deleted'
             ]);
-            
-        } catch (Exception $e){
+        } catch (Exception $e) {
             return response()->json($e);
         }
     }
